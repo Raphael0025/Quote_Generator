501fee76bd65df440c439a8dccb46b36c97cc17b
Raphael Isla <rpbisla0025@gmail.com>
2023-06-29 19:08:39 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: '',
+      colors: [],
+    }
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='quote-box'>
+        <div id='text'></div>
+        <div id='author'></div>
+        <div id='new-quote'></div>
+        <div id='tweet-quote'></div>
+        
+      </div>
     </div>
-  );
+  );}
 }
 
 export default App;
